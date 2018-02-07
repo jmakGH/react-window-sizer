6a34d3f6617dfa4f38c447e41556ad05982f7b48
Justin Mak <j.mak.dev@gmail.com>
2018-02-06 22:12:28 -0800

    Remove minification from build process

--- webpack.config.js
+++ webpack.config.js
@@ -23,23 +23,4 @@ module.exports = {
   externals: {
     react: 'commonjs react',
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        // Disabled because of an issue with Uglify breaking seemingly valid code:
-        // https://github.com/facebookincubator/create-react-app/issues/2376
-        // Pending further investigation:
-        // https://github.com/mishoo/UglifyJS2/issues/2011
-        comparisons: false,
-      },
-      mangle: {
-        safari10: true,
-      },
-      output: {
-        comments: false,
-      },
-      sourceMap: false,
-    }),
-  ],
 };
